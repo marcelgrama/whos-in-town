b71c585d07d38955ca0eb6534ff64598b6f89053
Marcel <aace375@yahoo.ro>
2023-05-26 16:32:52 +0300

    refactor useArtistEvents hook

--- src/hooks/useArtistEvents.ts
+++ src/hooks/useArtistEvents.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import { Artist, Event } from "../types";
 
@@ -8,29 +8,38 @@ const useArtistEvents = (artistName: string) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchArtistEvents = async () => {
-      try {
-        setIsLoading(true);
-        setError(null);
+  const fetchArtistEvents = useCallback(() => {
+    setIsLoading(true);
+    setError(null);
 
-        const { data: artistData } = await axios.get<Artist>(
-          `${process.env.REACT_APP_BASE_URL}artists/${artistName}?app_id=${process.env.REACT_APP_APP_ID}`
-        );
-        setArtist(artistData);
+    const artistPromise = axios.get<Artist>(
+      `${process.env.REACT_APP_BASE_URL}artists/${artistName}?app_id=${process.env.REACT_APP_APP_ID}`
+    );
+    const eventsPromise = axios.get<Event[]>(
+      `${process.env.REACT_APP_BASE_URL}artists/${artistName}/events?app_id=${process.env.REACT_APP_APP_ID}`
+    );
 
-        const { data: eventsData } = await axios.get<Event[]>(
-          `${process.env.REACT_APP_BASE_URL}artists/${artistName}/events?app_id=${process.env.REACT_APP_APP_ID}`
-        );
-        setEvents(eventsData);
-
+    Promise.allSettled([artistPromise, eventsPromise])
+      .then((results) => {
+        results.forEach((result) => {
+          if (result.status === "fulfilled") {
+            const { value } = result;
+            if (value.config.url && value.config.url.includes("events")) {
+              setEvents(value.data as Event[]);
+            } else {
+              setArtist(value.data as Artist | null);
+            }
+          }
+        });
         setIsLoading(false);
-      } catch (error) {
+      })
+      .catch(() => {
         setIsLoading(false);
         setError("Failed to fetch artist and events data");
-      }
-    };
+      });
+  }, [artistName]);
 
+  useEffect(() => {
     artistName && fetchArtistEvents();
   }, [artistName]);
 
